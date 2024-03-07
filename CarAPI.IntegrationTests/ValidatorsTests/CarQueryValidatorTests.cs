a6aad5843069f89c1edc52bfad416ddd583bf688
Kamil Surlas <ks303231@student.polsl.pl>
2024-03-07 14:46:49 +0100

    Validators tests

---
+++ CarAPI.IntegrationTests/ValidatorsTests/CarQueryValidatorTests.cs
@@ -0,0 +1,94 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Models.Validators;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace CarAPI.IntegrationTests.ValidatorsTests
+{
+    public class CarQueryValidatorTests
+    {
+        public static IEnumerable<object[]> GetSampleValidData()
+        {
+            var list = new List<Query>()
+        {
+            new Query()
+            {
+                PageNumber = 1,
+                PageSize = 5
+            },
+            new Query()
+            {
+                PageNumber = 2,
+                PageSize = 10
+            },
+             new Query()
+            {
+                PageNumber = 3,
+                PageSize = 15
+            },
+            new Query()
+            {
+                PageNumber = 3,
+                PageSize = 15,
+                SortBy = nameof(Car.BrandName)
+            },
+
+            new Query()
+            {
+                PageNumber = 1,
+                PageSize = 5,
+                SortBy = nameof(Car.Mileage)
+            },
+        };
+
+            return list.Select(e => new object[] { e });          
+        }
+        public static IEnumerable<object[]> GetSampleInvalidData()
+        {
+            var list = new List<Query>()
+        {
+            new Query()
+            {
+                PageNumber = 0,
+                PageSize = 5
+            },
+            new Query()
+            {
+                PageNumber = 2,
+                PageSize = 11
+            },
+             new Query()
+            {
+                PageNumber = 1,
+                PageSize = 5,
+                SortBy = nameof(Car.BodyType)
+            },          
+        };
+
+            return list.Select(e => new object[] { e });
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleValidData))]
+        public void Validate_ForValidModel_ReturnsSuccess(Query model)
+        {
+            var validator = new CarQueryValidator();
+           
+            var result = validator.TestValidate(model);
+
+            result.ShouldNotHaveAnyValidationErrors();
+            
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidData))]
+        public void Validate_ForInvalidModel_ReturnsFailure(Query model)
+        {
+            var validator = new CarQueryValidator();
+
+            var result = validator.TestValidate(model);
+
+            result.ShouldHaveAnyValidationError();
+
+        }
+    }
+}
---
+++ CarAPI.IntegrationTests/ValidatorsTests/RegisterUserDtoValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Models.Validators;
+using CarAPI.Seeder;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CarAPI.IntegrationTests.ValidatorsTests
+{
+    public class RegisterUserDtoValidatorTests
+    {
+        private CarDbContext _dbContext;
+        public RegisterUserDtoValidatorTests()
+        {
+            var builder = new DbContextOptionsBuilder<CarDbContext>();
+            builder.UseInMemoryDatabase("TestCarDb", db => db.EnableNullChecks(false));
+
+            _dbContext = new CarDbContext(builder.Options);
+
+            SeedTestDb();
+        }
+        public static IEnumerable<object[]> GetSampleValidData()
+        {
+            var list = new List<RegisterUserDto>()
+        {
+            new RegisterUserDto()
+            {
+                Email = "test@test.com",
+                Password = "password123test",
+                ConfirmPassword = "password123test"
+            },
+            new RegisterUserDto()
+            {
+                 Email = "testTest@test.com",
+                Password = "password123test2",
+                ConfirmPassword = "password123test2"
+            }
+            };
+            return list.Select(e => new object[] { e });
+
+        }
+        public static IEnumerable<object[]> GetSampleInvalidData()
+        {
+            
+           var list = new List<RegisterUserDto>()
+        {
+            new RegisterUserDto()
+            {
+                Email = "test2@test.com",
+                Password = "password123test",
+                ConfirmPassword = "password123test"
+            },
+            new RegisterUserDto()
+            {
+                 Email = "testTest@test.com",
+                Password = "password123test2",
+                ConfirmPassword = "123passwordtest2"
+            }
+            };
+            return list.Select(e => new object[] { e });
+
+        }
+        private void SeedTestDb()
+        {
+            var testUsers = new List<User>()
+            {
+                new User()
+                {
+                    Email = "test2@test.com"
+                },
+                new User()
+                {
+                 Email = "test3@test.com"
+                }
+            };
+
+            _dbContext.Users.AddRange(testUsers);
+            _dbContext.SaveChanges();
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleValidData))]
+        public void Validate_ForValidModel_ReturnsSuccess(RegisterUserDto model)
+        {       
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidData))]
+        public void Validate_ForInvalidModel_ReturnsSuccess(RegisterUserDto model)
+        {
+            var validator = new RegisterUserDtoValidator(_dbContext);
+
+            var result = validator.TestValidate(model);
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
